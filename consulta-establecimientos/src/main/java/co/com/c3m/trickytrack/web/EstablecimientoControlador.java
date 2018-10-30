d28a1e77822d12a3f227bb073764b5bfc58d3996
Camilo <camilo.nino@c3m.com.co>
2018-10-29 22:23:59 -0500

    Depuracion del servicio de encontrar establecimientos

--- consulta-establecimientos/src/main/java/co/com/c3m/trickytrack/web/EstablecimientoControlador.java
+++ consulta-establecimientos/src/main/java/co/com/c3m/trickytrack/web/EstablecimientoControlador.java
@@ -3,6 +3,8 @@ package co.com.c3m.trickytrack.web;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
+import java.util.Locale;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -26,23 +28,27 @@ public class EstablecimientoControlador {
 			@RequestParam Double latitud,
 			@RequestParam Boolean validaHorario,
 			@RequestParam List<String> generos){
-		List<String> parametros = new ArrayList<>();
-		StringBuilder qry = new StringBuilder("SELECT * FROM ESTABLECIMIENTO WHERE 1=1 ");
+		List<Object> parametros = new ArrayList<>();
+		StringBuilder qry = new StringBuilder("SELECT * FROM establecimiento WHERE 1=1 ");
 
 		if ((longitud!=null && latitud==null) || 
 				(latitud!=null && longitud==null)) {
 			throw new RuntimeException("Se debe ingresar latitud y longitud");
 		}else if (longitud!=null && latitud!=null){
 			qry.append("AND ID IN (SELECT ID FROM (SELECT id, ")
-			.append("(6371 * acos ( cos ( radians(4.652052) )")
-			.append("* cos( radians( latitud ) ) ")
-			.append("* cos( radians( longitud ) - radians(-74.0555773) ) ")
-			.append(" sin ( radians(4.652052) ) ")
-			.append(" * sin( radians( latitud ) ) )) AS distance ")
-			.append(" FROM establecimiento ")
-			.append(" HAVING distance < 1 ")
-			.append(" ORDER BY distance")
-			.append(" LIMIT 0 , 20) AS TMP) ");
+			.append("(6371*acos(cos(radians(?)) ")
+			.append("*cos(radians(latitud)) ")
+			.append("*cos(radians(longitud)-radians(?)) ")
+			.append("+sin(radians(?)) ")
+			.append("*sin(radians(latitud)))) AS distance ")
+			.append("FROM establecimiento ")
+			.append("HAVING distance < 1 ")
+			.append("ORDER BY distance ")
+			.append("LIMIT 0 , 20) AS TMP) ");
+			
+			parametros.add(latitud);
+			parametros.add(longitud);
+			parametros.add(latitud);
 		}
 
 		if (nombre!=null && !nombre.isEmpty()) {
@@ -50,36 +56,42 @@ public class EstablecimientoControlador {
 			parametros.add("%"+nombre+"%");
 		}
 		if (null!=validaHorario && validaHorario) {
-			Calendar cal = Calendar.getInstance();
+			Calendar cal = Calendar.getInstance(
+					TimeZone.getTimeZone("UTC-5"), 
+					Locale.forLanguageTag("es"));
 			int dia = cal.get(Calendar.DAY_OF_WEEK);
-
+			dia--;
 			switch (dia) {
 			case 1:
-				qry.append(" AND LUNES=TRUE ");
+				qry.append(" AND domingo=TRUE ");
 				break;
 			case 2:
-				qry.append(" AND MARTES=TRUE ");
+				qry.append(" AND lunes=TRUE ");
 				break;
 			case 3:
-				qry.append(" AND MIERCOLES=TRUE ");
+				qry.append(" AND martes=TRUE ");
 				break;
 			case 4:
-				qry.append(" AND JUEVES=TRUE ");
+				qry.append(" AND miercoles=TRUE ");
 				break;
 			case 5:
-				qry.append("AND VIERNES=TRUE ");
+				qry.append(" AND jueves=TRUE ");
 				break;
 			case 6:
-				qry.append(" AND SABADO=TRUE ");
+				qry.append("AND viernes=TRUE ");
 				break;
 			case 7:
-				qry.append(" AND DOMMINGO=TRUE ");
+				qry.append(" AND sabado=TRUE ");
 				break;
 			default:
 				break;
 			}
 
-			qry.append("AND ? BETWEEN HORA_INICIO AND HORA_CIERRE ");
+			qry.append("AND TIME(?) BETWEEN hora_inicio AND '23:59:59' ")
+			.append("OR TIME(?) BETWEEN '00:00:00' AND hora_cierre ");
+
+			parametros.add(cal.getTime());
+			parametros.add(cal.getTime());
 		}
 
 		return jdbcTemplate.query(qry.toString(), parametros.toArray(), new EstablecimientoRowmapper() );
--- consulta-establecimientos/src/main/resources/DDL.sql
+++ consulta-establecimientos/src/main/resources/DDL.sql
@@ -15,21 +15,21 @@ CREATE TABLE `establecimiento` (
   `domingo` bit(1) NOT NULL DEFAULT 1,
   PRIMARY KEY (`id`),
   UNIQUE KEY `UKrk31lb3hy2f4g7x5jt5kchuny` (`nombre`,`longitud`,`latitud`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 CREATE TABLE `genero_musical` (
   `id` bigint(20) NOT NULL,
   `nombre` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 CREATE TABLE `establecimiento_generos` (
   `establecimiento_id` bigint(20) NOT NULL,
   `generos_id` bigint(20) NOT NULL,
   KEY `FKmb89y3uyn5ip5dp90lh1mravb` (`generos_id`),
   KEY `FKsm6lxtjeveijy4t7bmvmgc1ou` (`establecimiento_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 CREATE TABLE `hibernate_sequence` (
   `next_val` bigint(20) DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
