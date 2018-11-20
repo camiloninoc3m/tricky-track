84ffb257a32831a3ca45c4a3d80442ddaab4f062
Camilo <camilo.nino@c3m.com.co>
2018-11-19 22:32:11 -0500

    Servicio de consulta de usuario

---
+++ principal/src/main/java/co/com/c3m/trickytrack/api/ConsultaUsuarioResDTO.java
@@ -0,0 +1,24 @@
+package co.com.c3m.trickytrack.api;
+
+public class ConsultaUsuarioResDTO {
+
+	private Long idEstablecimiento;
+	
+	private Long idCliente;
+
+	public Long getIdEstablecimiento() {
+		return idEstablecimiento;
+	}
+
+	public void setIdEstablecimiento(Long idEstablecimiento) {
+		this.idEstablecimiento = idEstablecimiento;
+	}
+
+	public Long getIdCliente() {
+		return idCliente;
+	}
+
+	public void setIdCliente(Long idCliente) {
+		this.idCliente = idCliente;
+	}
+}
--- principal/src/main/java/co/com/c3m/trickytrack/web/RegistroClienteControlador.java
+++ principal/src/main/java/co/com/c3m/trickytrack/web/RegistroClienteControlador.java
@@ -1,18 +1,25 @@
 package co.com.c3m.trickytrack.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import co.com.c3m.trickytrack.api.ConsultaUsuarioResDTO;
 import co.com.c3m.trickytrack.api.RegistroClienteResDTO;
 import co.com.c3m.trickytrack.dominio.Cliente;
+import co.com.c3m.trickytrack.dominio.Establecimiento;
 import co.com.c3m.trickytrack.repositorio.ClienteDAO;
+import co.com.c3m.trickytrack.repositorio.EstablecimientoDAO;
 
 @RestController
 public class RegistroClienteControlador {
 
+	@Autowired
+	private EstablecimientoDAO establecimientoDao;
+	
 	@Autowired
 	private ClienteDAO dao;
 	
@@ -28,6 +35,24 @@ public class RegistroClienteControlador {
 		dao.save(cliente);
 		RegistroClienteResDTO respuesta = new RegistroClienteResDTO();
 		respuesta.setId(cliente.getId());
+		return respuesta;
+	}
+	
+	@RequestMapping(path="/usuario/celular/{celular}", method=RequestMethod.GET)
+	public ConsultaUsuarioResDTO consultarUsuario(
+			@PathVariable("celular") String celular) {
+		ConsultaUsuarioResDTO respuesta = new ConsultaUsuarioResDTO();
+		Cliente cliente = dao.findByCelular(celular);
+		Establecimiento establecimiento = establecimientoDao.findByCelular(celular);
+		
+		if (cliente!=null) {
+			respuesta.setIdCliente(cliente.getId());
+		}
+		
+		if (establecimiento!=null) {
+			respuesta.setIdEstablecimiento(establecimiento.getId());
+		}
+		
 		return respuesta;
 	}
 }
